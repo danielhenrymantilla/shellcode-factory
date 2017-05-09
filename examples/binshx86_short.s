bf5cadc022ebf30959292443c190133ff85febd8
danielhenrymantilla <daniel.henry.mantilla@gmail.com>
2017-05-09 22:13:17 +0200

    minor example changes

--- examples/binshx86_short.s
+++ examples/binshx86_short.s
@@ -10,6 +10,6 @@ _start:
 	movl %esp, %ebx		# "/bin//sh" addr
 	movl %edx, %ecx		# argv = %ecx = NULL 
 	int $0x80		# syscall
- 
-# char shellcode[] = "\x31\xc0\xb0\x0b\x99\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xd1\xcd\x80";
+
+# char shellcode[] = "\x31\xc0\xb0\x0b\x99\x52\x68//sh\x68/bin\x89\xe3\x89\xd1\xcd\x80";
 # /* Total: 22 bytes. */
