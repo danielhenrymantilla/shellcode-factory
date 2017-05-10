1e8afc41fdd4727be0a203beaef623ab1e1104b3
danielhenrymantilla <daniel.henry.mantilla@gmail.com>
2017-05-11 00:24:14 +0200

    Reordered basic shellcode example instructions for clarity

--- shellcode.s
+++ shellcode.s
@@ -2,12 +2,12 @@
 .globl _start
 _start:
 	xorl %eax, %eax
-	cdq
 	movb $11, %al
 	jmp binsh
 	back:
-	movl %edx, %ecx
 	movl (%esp), %ebx
+	cdq
+	movl %edx, %ecx
 	movb %dl, 0x7(%ebx)
 	int $0x80
 binsh:
